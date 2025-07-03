df1cfab27049566219dd533f76d24732213e86b6
Cedric RUGAMBA <rugced23@gmail.com>
2025-07-03 13:40:53 +0200

    update the api link

--- scripts/main.js
+++ scripts/main.js
@@ -427,7 +427,7 @@ document.addEventListener('DOMContentLoaded', () => {
             
             try {
                 // Send the data to the API
-                const response = await fetch('https://backend-production-ee6bf.up.railway.app/api/submit/contact', {
+                const response = await fetch('shttps://bff.corecrest.tech/api/submit/contact', {
                     method: 'POST',
                     headers: {
                         'accept': 'application/json',
